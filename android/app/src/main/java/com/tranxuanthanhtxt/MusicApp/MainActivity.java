951772c434834a58bdfcfe857ff87e676f06f960
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-06-25 08:32:35 +0700

    refactor: remove capacitor-community-sqlite dependency from build configuration

--- android/app/src/main/java/com/tranxuanthanhtxt/MusicApp/MainActivity.java
+++ android/app/src/main/java/com/tranxuanthanhtxt/MusicApp/MainActivity.java
@@ -2,15 +2,11 @@ package com.tranxuanthanhtxt.MusicApp;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.database.sqlite.CapacitorSQLitePlugin;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Register the SQLite plugin
-        this.registerPlugin(CapacitorSQLitePlugin.class);
     }
 }
