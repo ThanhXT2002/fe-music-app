998f236ff8991fa131e4646acb6603a5e2e78cfd
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-07-18 11:44:38 +0700

    fix: correct Facebook login protocol scheme in strings.xml and clean up MainActivity

--- android/app/src/main/java/xtmusic/tranxuanthanhtxt/com/MainActivity.java
+++ android/app/src/main/java/xtmusic/tranxuanthanhtxt/com/MainActivity.java
@@ -3,7 +3,6 @@ package xtmusic.tranxuanthanhtxt.com;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
-import com.getcapacitor.Plugin;
 
 public class MainActivity extends BridgeActivity {
 
@@ -11,12 +10,5 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(FirebaseAuthenticationPlugin.class);
-        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-            // Add your plugins here
-            // Ex: add(TotallyAwesomePlugin.class);
-            add(com.getcapacitor.community.facebooklogin.FacebookLoginPlugin.class);
-        }});
-
-
     }
 }
