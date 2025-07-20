63d484293def6b9d165708e8ca92b8476a49a949
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-07-20 14:44:24 +0700

    refactor: update environment variables in generate-env.js to use empty defaults

--- generate-env.js
+++ generate-env.js
@@ -9,39 +9,39 @@ if (!fs.existsSync(envDir)) {
 // Development environment file (default values)
 const devEnvContent = `export const environment = {
   production: false,
-  apiUrl: '${process.env.DEV_API_URL || 'http://localhost:8000/api'}',
+  apiUrl: '${process.env.DEV_API_URL || ''}',
   appVersion: require('../../package.json').version,
   firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY || 'AIzaSyDbBfc6kNnVILMIQsYN_q83HVDDwNrFBuo'}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || 'txt-system-90788.firebaseapp.com'}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID || 'txt-system-90788'}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || 'txt-system-90788.firebasestorage.app'}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || '403716398920'}',
-    appId: '${process.env.FIREBASE_APP_ID || '1:403716398920:web:daf2be700aa20f9cba5d7d'}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || 'G-TVYQ5L8G7B'}',
+    apiKey: '${process.env.FIREBASE_API_KEY || ''}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ''}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID || ''}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ''}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}',
+    appId: '${process.env.FIREBASE_APP_ID || ''}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || ''}',
   },
   appName: 'XTMusic',
   emailSupport: 'tranxuanthanhtxt2002@gmail.com',
-  fbAppId: '24147476401529487',
+  fbAppId: '${process.env.FB_APP_ID || ''}',
 };`;
 
 // Production environment file
 const prodEnvContent = `export const environment = {
   production: true,
-  apiUrl: '${process.env.API_URL || 'http://api-music.tranxuanthanh.vn/api/v3'}',
+  apiUrl: '${process.env.API_URL || ''}',
   appVersion: require('../../package.json').version,
   firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY || 'AIzaSyDbBfc6kNnVILMIQsYN_q83HVDDwNrFBuo'}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || 'txt-system-90788.firebaseapp.com'}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID || 'txt-system-90788'}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || 'txt-system-90788.firebasestorage.app'}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || '403716398920'}',
-    appId: '${process.env.FIREBASE_APP_ID || '1:403716398920:web:daf2be700aa20f9cba5d7d'}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || 'G-TVYQ5L8G7B'}',
+    apiKey: '${process.env.FIREBASE_API_KEY || ''}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ''}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID || ''}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ''}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}',
+    appId: '${process.env.FIREBASE_APP_ID || ''}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || ''}',
   },
   appName: 'XTMusic',
   emailSupport: 'tranxuanthanhtxt2002@gmail.com',
-  fbAppId: '24147476401529487',
+  fbAppId: '${process.env.FB_APP_ID || ''}',
 };`;
 
 // Write both environment files
