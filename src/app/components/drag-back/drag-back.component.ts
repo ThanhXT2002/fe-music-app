f86efa2877205a0abb95db5526f9a1dce4647777
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-07-30 01:49:17 +0700

    feat: update version to 1.2.4 in package.json and package-lock.json; refactor drag-back component for improved styling and native platform detection

--- src/app/components/drag-back/drag-back.component.ts
+++ src/app/components/drag-back/drag-back.component.ts
@@ -1,14 +1,18 @@
-import { Location } from '@angular/common';
+import { Location, NgClass } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
 
 
 @Component({
   selector: 'app-drag-back',
   templateUrl: './drag-back.component.html',
   styleUrls: ['./drag-back.component.scss'],
+  imports: [NgClass]
 })
 export class DragBackComponent  implements OnInit {
   private location = inject(Location);
+  isNative = Capacitor.isNativePlatform()
+
 
   constructor() { }
 
